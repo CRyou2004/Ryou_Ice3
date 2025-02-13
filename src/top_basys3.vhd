9209d5161001d11e62108c12bf8f339611a26d8f
CRyou2004 <Chaimin2004@gmail.com>
2025-02-13 11:40:05 -0700

    Ice3 submission

--- src/top_basys3.vhd
+++ src/top_basys3.vhd
@@ -1,4 +1,3 @@
---+----------------------------------------------------------------------------
 --| 
 --| DESCRIPTION   : This file implements the top level module for a BASYS 
 --|
@@ -56,13 +55,30 @@ end top_basys3;
 architecture top_basys3_arch of top_basys3 is 
 	
     -- declare the component of your top-level design
+    component ripple_adder is
+        port (
+            A     : in std_logic_vector(3 downto 0);
+            B     : in std_logic_vector(3 downto 0);
+            Cin   : in std_logic;
+            S     : out std_logic_vector(3 downto 0);
+            Cout  : out std_logic
+            );
+        end component ripple_adder;
 
     -- declare any signals you will need	
   
 begin
 	-- PORT MAPS --------------------
-   
+   ripple_adder_0: ripple_adder
+    port map(
+        A     => sw(4 downto 1),
+        B     => sw(15 downto 12),
+        Cin   => sw(0),
+        S     => led(3 downto 0),
+        Cout  => led(15)
+    );
 	---------------------------------
+	
 	
 	-- CONCURRENT STATEMENTS --------
 	led(14 downto 4) <= (others => '0'); -- Ground unused LEDs
